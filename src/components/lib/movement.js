6baf841f209596516b759e938938515315aff9ae
Adhavan Sivaraj <adhavan.sivaraj16@gmail.com>
2023-05-12 15:24:21 +0530

    modularising code

--- src/components/lib/movement.js
+++ src/components/lib/movement.js
@@ -1,15 +1,15 @@
 // Geospatial librarie
 import * as turf from "@turf/turf";
-
+import maplibre from "maplibre-gl";
 // Pans and zooms to next story point
-export function moveTo( coords, zoom, layer, map) {
+export function moveTo( coords, zoom, pitch=60, bearing=30,  layer, map) {
   map.flyTo({
     center: coords,
     zoom: zoom,
-    pitch: 60, // pitch in degrees
-    bearing: 30, // bearing in degrees
-    speed: 0.5,
-    curve: 1,
+    pitch: pitch, // pitch in degrees
+    bearing: bearing, // bearing in degrees
+    speed: 0.6,
+    curve: 1.3,
     easing: function (t) {
       return t;
     },
@@ -22,8 +22,7 @@ export function moveTo( coords, zoom, layer, map) {
 // Scatter effect to illustrate scale and disorient user
 // Scatter effect to illustrate scale and disorient user
 export function makeGrid(map) {
-  console.log("Scatter activating"
-  )
+  // If layer does not exist, calculate and display
   let layer = map.getSource("atRiskVillages100mts");
   const features = map.querySourceFeatures("atRiskVillages100mts", {
     sourceLayer: layer.sourceLayer,
@@ -74,7 +73,54 @@ export function makeGrid(map) {
     }
   );
 
-  // Updates the source with the new data
-  map.getSource("atRiskVillages100mts").setData(geojsonLayer);
+  // Disable previous layer and add as new layer
+  map.setLayoutProperty("atRiskVillages100mts", "visibility", "none");
+  map.addSource("atRiskVillages100mtsScattered", {
+    type: "geojson",
+    data: geojsonLayer,
+  });
+  map.addLayer({
+    id: "atRiskVillages100mtsScattered",
+    type: "fill",
+    source: "atRiskVillages100mtsScattered"
+    });
+  
+  map.addLayer({
+    id: "atRiskVillage100mtsLineScatteredLine",
+    type: "line",
+    source: "atRiskVillages100mtsScattered",
+    paint: {
+      "line-color": "black",
+      "line-width": 3,
+      "line-opacity": 0.5,
+    },
+  });
+
+  map.on("click", "atRiskVillages100mtsScattered", function (e) {
+    map.flyTo({
+      center: e.lngLat,
+      zoom: 12,
+      essential: true, // this animation is considered essential with respect to prefers-reduced-motion
+    });
+  });
+  const popup = new maplibre.Popup();
+  map.on("mouseenter", "atRiskVillages100mtsScattered", function (e) {
+    map.getCanvas().style.cursor = "pointer";
+    
+    popup
+      .setLngLat(e.lngLat)
+      .setHTML(
+        e.features[0].properties.name +
+          "<br> Total Population: " +
+          e.features[0].properties.tot_p
+      )
+      .addTo(map);
+  });
+  map.on("mouseleave", "atRiskVillages100mtsScattered", function () {
+    map.getCanvas().style.cursor = "";
+    popup.remove();
+  });
+  
+  //map.getSource("atRiskVillages100mts").setData(geojsonLayer);
   // Style the layer
 }
